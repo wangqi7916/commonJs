5bf3d391b9c24e26eb719eefd3c90d85687c1245
wangqi <1191923306@qq.com>
2019-10-09 10:15:23 +0800

    时间格式化

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,5 +1,26 @@
+// export default function dateFormat(fmt, date) {
+//   let ret;
+//   let opt = {
+//     "Y+": date.getFullYear().toString(), // 年
+//     "m+": (date.getMonth() + 1).toString(), // 月
+//     "d+": date.getDate().toString(), // 日
+//     "H+": date.getHours().toString(), // 时
+//     "M+": date.getMinutes().toString(), // 分
+//     "S+": date.getSeconds().toString() // 秒
+//   }
+
+//   for (let k in opt) {
+//     ret = new RegExp("(" + k + ")").exec(fmt);
+//     console.log(ret)
+//     if (ret) {
+//       fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+//     }
+//   }
+//   return fmt
+// }
+
 class Method {
-  // fmt "YYYY-mm-dd HH:MM"
+  // fmt格式
   dateFormat(fmt, date) { // 时间格式化
     let ret;
     let opt = {
@@ -21,8 +42,5 @@ class Method {
     return fmt
   }
 }
-
 window.$ = new Method()
-
 export default $
-
